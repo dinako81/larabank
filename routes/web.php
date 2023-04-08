048ebf9125acddc6028d19aa00b8fb404cb4136f
dinako81 <dina.koliata@gmail.com>
2023-04-09 01:11:05 +0300

    model, create

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\ClientController as CL;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,14 @@ Route::get('/hello/client', [HelloController::class, 'hello'])->name('client');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::prefix('clients')->name('clients-')->group(function () {
+    Route::get('/', [CL::class, 'index'])->name('index');
+    Route::get('/create', [CL::class, 'create'])->name('create');
+    Route::post('/create', [CL::class, 'store'])->name('store');
+    Route::get('/{client}', [CL::class, 'show'])->name('show');
+    Route::get('/edit/{client}', [CL::class, 'edit'])->name('edit');
+    Route::put('/edit/{client}', [CL::class, 'update'])->name('update');
+    Route::delete('/delete/{client}', [CL::class, 'destroy'])->name('delete');
+});
